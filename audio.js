76b54f6260b00d7447d0200495e64122185b761e
KUNALRAWAT789 <rawat98766@gmail.com>
2026-03-13 23:48:25 +0530

    Add files via upload

--- audio.js
+++ audio.js
@@ -1,6 +1,14 @@
-const audio_context = new AudioContext();
-const button = document.querySelector('Audio_processing');
+const audio_context = new AudioContext({
+    sampleRate:16000
+});
+const button = document.querySelector('#Audio_processing');
 
-button.addEventListener('click',()=>{
+button.addEventListener('click',async ()=>{
     if(audio_context.state === "suspended") audio_context.resume();
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+    const source = audio_context.createMediaStreamSource(stream);
+
+    await audioContext.audioWorklet.addModule('audio_processor.js'); 
+    const nodeInstance = new AudioWorkletNode(audioContext, 'shack-audio-processor');
+    
 });
